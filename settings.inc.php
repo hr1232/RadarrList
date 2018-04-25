e98e62cf4f0b052dee34ca70786a987c8429ddaf
hr1232 <email@heikorichter.name>
2018-04-25 16:26:30 +0200

    Update settings.inc.php

--- settings.inc.php
+++ settings.inc.php
@@ -1,7 +1,7 @@
 <?php
 
   // tmdb api to use
-  $API = 'abcdefghijklmnopqrstuvwxyz1234567890'
+  $API = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODBkNTJlMzI1YjkwNmVlMTVjOGE5MDViMmVjNThhMCIsInN1YiI6IjU2ODc0MWI4YzNhMzY4NGJlOTAyMDRiMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iJhJ17cAUpcYQojVan9HAUdpQbsvDw3-lH2_7CcRqkw'
   
   // get settings from file
   if(is_file('/etc/radarrlist/settings.inc.php'))
