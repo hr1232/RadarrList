3d1e1318b04141d0a9647041424bb528e60471e7
hr1232 <email@heikorichter.name>
2018-04-25 14:45:13 +0200

    Create settings.inc.php

---
+++ settings.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+  // tmdb api to use
+  $API = 'abcdefghijklmnopqrstuvwxyz1234567890'
+  
+  // get settings from file
+  if(is_file('/etc/radarrlist/settings.inc.php'))
+    include('/etc/radarrlist/settings.inc.php');
+
+?>
