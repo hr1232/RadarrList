1bf7bf5dd3a506ecf5da7835471d670645888eeb
hr1232 <email@heikorichter.name>
2018-04-29 21:20:42 +0200

    commit

--- cron-daily.php
+++ cron-daily.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 
-  require(dirname(__FILE__)."/settings.inc.php");
+  require(dirname(__FILE__)."/settings.inc.php"); 
   require(dirname(__FILE__)."/functions.inc.php");
 
   // connect to database
